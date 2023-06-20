9e54edb5ce48c002961127816bd9623ef407f280
yanqinghao <woshiyanqinghao@gmail.com>
2023-06-20 22:32:54 +0800

    save log

--- utils/start.sh
+++ utils/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-nohup /usr/bin/xray -config /etc/xray/config.json > /dev/null 2>&1 &
+nohup /usr/bin/xray -config /etc/xray/config.json > xray.log 2>&1 &
 
-nohup uvicorn main:app --reload --host 0.0.0.0 --port 5000 > /dev/null 2>&1 &
+nohup uvicorn main:app --reload --host 0.0.0.0 --port 5000 > openai-proxy.log 2>&1 &
 
 sleep 5
 
